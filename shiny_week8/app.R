ca799fb8d4bca6429e3c523ad47db011716dd442
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-18 11:21:57 -0500

    Part 3 - Revise

--- shiny_week8/app.R
+++ shiny_week8/app.R
@@ -27,17 +27,17 @@ ui = fluidPage(
 server = function(input, output){
   # Input: Load RDS file making rmd file
   shinyapp_data <- readRDS("week8_data.rds")
-  # Output: If function
-  output$plot = renderPlot({# By using the if function, when users select Exclusion, app excludes participants that completed the assessment before July 1, 2017.
-    if(input$date_include == "Exclude") {
-      shinyapp_data %>% 
-        filter(timeEnd >= "2017-07-01 00:00:00")
-    }  else {}
+  # Output: By using the if function, when users select Exclusion, app excludes participants that completed the assessment before July 1, 2017.
+  output$plot <- renderPlot({
+    if(input$date_include == c("Exclude")) {
+      shinyapp_data <- shinyapp_data %>% 
+        filter(timeEnd >= ymd_hms("2017-07-01 00:00:00"))
+    } 
     # By using the if function, when users select Female or male, app shows Female or male according they selected.
-    if(input$gender != "All"){ 
-      shinyapp_data %>% 
+    if(input$gender == c("Male") | input$gender == c("Female")) { 
+      shinyapp_data <- shinyapp_data %>% 
         filter(gender == input$gender)
-    }  else {}
+    } 
     # By using the if and else function, when users select Suppress Error Band, app doesn't show the shaded error band around the regression line.
     if(input$error_band == "Display Error Band") {
       shinyapp_data %>% 
@@ -52,4 +52,5 @@ server = function(input, output){
         geom_smooth(method = "lm", color = "purple", se = FALSE) + 
         labs(x = "Average scores of Q1 ~ Q6", y = "Average scores of Q8 ~ Q10")}})}
 
+# Run shiny spp
 shinyApp(ui = ui , server = server)
